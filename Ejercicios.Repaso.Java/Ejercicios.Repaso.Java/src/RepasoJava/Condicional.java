d298aebdd4606c415694076464fa0a7dd50194d4
Peybool <pablomartin9696@gmail.com>
2018-07-17 13:58:01 +0200

    Ejers 1 y 2

--- Ejercicios.Repaso.Java/Ejercicios.Repaso.Java/src/RepasoJava/Condicional.java
+++ Ejercicios.Repaso.Java/Ejercicios.Repaso.Java/src/RepasoJava/Condicional.java
@@ -4,9 +4,148 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Condicional {
-
-	public static void main (String[] args) {
+	
+	public static void mainMesCorrecto10(String[] args) {
 		
+		 Scanner sc = new Scanner(System.in);
+	        int mes;
+	        System.out.print("Introduzca número de mes: ");
+	        mes =  sc.nextInt();
+	        if(mes < 1 || mes > 12) //se comprueba que el valor del mes es correcto
+	            System.out.println("Mes incorrecto");
+	        else{  //si el mes es correcto
+	            switch(mes){  //se muestra el nombre mediante una instrucción switch
+	                case 1: System.out.print("Enero");
+	                        break;
+	                case 2: System.out.print("Febrero");
+	                        break;
+	                case 3: System.out.print("Marzo");
+	                        break;
+	                case 4: System.out.print("Abril");
+	                        break;
+	                case 5: System.out.print("Mayo");
+	                        break;
+	                case 6: System.out.print("Junio");
+	                        break;
+	                case 7: System.out.print("Julio");
+	                        break;
+	                case 8: System.out.print("Agosto");
+	                        break;
+	                case 9: System.out.print("Septiembre");
+	                        break;
+	                case 10: System.out.print("Octubre");
+	                        break;
+	                case 11: System.out.print("Noviembre");
+	                        break;
+	                case 12: System.out.print("Diciembre");
+	                        break;
+	            }
+	            // mostrar si es un mes de 30, 31 0 28 días
+	            if(mes == 4 || mes == 6 || mes == 9 || mes == 11)       
+	               System.out.println(" es un mes de 30 días");
+	            else if(mes == 2)
+	                     System.out.println(" es un mes de 28 días");
+	                   else
+	                      System.out.println(" es un mes de 31 días");
+	        }
+	}
+	
+	public static void mainFormatoFecha9(String[] args) {
+		
+		int H, M, S;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca la hora: ");
+		H = sc.nextInt();
+		System.out.println("Introduzca los minutos: ");
+		M = sc.nextInt();
+		System.out.println("Introduzca los segundo: ");
+		S = sc.nextInt();
+		
+		if(H>=0 && H<24 && M>=0 && M<60 && S>=0 && S<60) {
+			System.out.println("Fecha en formato correcto");
+		}else {
+			System.out.println("El formato de la fecha no es el correcto");
+		}
+			
+		
+	}
+	
+	public static void mainNumeroMayor8(String[] args) {
+		
+		int num1, num2, num3;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduce el primer dato: ");
+		num1 = sc.nextInt();
+		System.out.println("Introduce el segundo dato: ");
+		num2 = sc.nextInt();
+		System.out.println("Introduce el tercer dato: ");
+		num3 = sc.nextInt();
+		
+		if (num1 > num2) {
+            if (num1 > num3) {
+                System.out.println("El mayor es: " + num1);
+            } else {
+                System.out.println("El mayor es: " + num3);
+            }
+        } else if (num2 > num3) {
+            System.out.println("El mayor es: " + num2);
+        } else {
+            System.out.println("El mayor es: " + num3);
+        }
+	}
+	
+	public static void mainExcepcionDivisionCero7/*Regulintxi*/(String[] args) {
+		
+		int num1, num2, division;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduce un numero para dividir: ");
+		num1 = sc.nextInt();
+		System.out.println("introduce un numero para dividir al primero:");
+		num2 = sc.nextInt();
+		
+		division = num1 / num2;
+		
+		try {
+			if (num2 == 0) {
+				System.out.println("El divisor no puede ser igual a 0");
+			}else {
+				System.out.println("El resultado de la division es " + division);
+			}
+		} catch (ArithmeticException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	/*
+	 * Scanner sc = new Scanner(System.in);
+        double dividendo, divisor;
+        System.out.print("Introduzca el dividendo: ");
+        dividendo = sc.nextDouble(); 
+        System.out.print("Introduzca el divisor: ");
+        divisor = sc.nextDouble(); 
+        if(divisor==0)
+           System.out.println("No se puede dividir por cero");    
+        else{
+            System.out.println(dividendo + " / " + divisor + " = " + dividendo/divisor);   
+            System.out.printf("%.2f / %.2f = %.2f %n" , dividendo, divisor , dividendo/divisor);
+        }*/
+
+	public static void mainCaracterNumero6(String[] args) throws IOException {
+		
+		char car;
+		
+		System.out.println("Introduce un caracter: ");
+		car = (char)System.in.read();
+		
+		if(Character.isDigit(car)) {
+			System.out.println("El caracter es un numero");
+		}else {
+			System.out.println("El caracter NO es un numero");
+		}
+			
 	}
 	
 	public static void mainCaracterMinusculo5/*NO FUNCIONA*/(String[] args) throws IOException {
